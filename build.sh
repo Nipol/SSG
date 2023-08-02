0e16573943addf6554436a3a51f468015c3532ee
Yoonsung <ys.choi@me.com>
2023-08-02 10:19:45 +0900

    chore fix

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ DENO_VERSION="v1.35.3"
 npm install -D tailwindcss
 curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=./deno-v1.35.3 sh -s v1.35.3
 NO_COLOR=1 DENO_VERSION=v1.35.3 \
-    ./deno-v1.35.3/bin/deno task tailwind:build \
+    npx tailwindcss -i main.css -o dist/build.css -m \
     && ./deno-v1.35.3/bin/deno task build \
