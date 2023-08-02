22452a5f72ac1852455ee01ec442fc1ce5d8be54
Yoonsung <ys.choi@me.com>
2023-08-02 10:18:20 +0900

    add Tailwind

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 DENO_VERSION="v1.35.3"
+npm install -D tailwindcss
 curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=./deno-v1.35.3 sh -s v1.35.3
 NO_COLOR=1 DENO_VERSION=v1.35.3 \
     ./deno-v1.35.3/bin/deno task tailwind:build \
