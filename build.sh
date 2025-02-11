a764f96984d200e3fc451748ecd4870c650d9efa
yoonsung <ys.choi@me.com>
2025-02-11 04:57:09 +0000

    Update build Script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-npm install tailwindcss @tailwindcss/cli playwright
-npx playwright install --with-deps chromium
+npm install tailwindcss @tailwindcss/cli playwright \
+&& npx playwright install --with-deps chromium
 curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=./deno sh -s v2.1.9
 
 NO_COLOR=1 npx @tailwindcss/cli -i main.css -o dist/build.css -m \
