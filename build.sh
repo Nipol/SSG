69d5c4228c1965dbc2819c17f96c303d45e4e604
yoonsung <ys.choi@me.com>
2025-02-16 22:44:40 +0900

    add playwright

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-npm install --unsafe-perm tailwindcss @tailwindcss/cli playwright \
-&& npx playwright install --with-deps chromium
-curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=./deno sh -s v2.1.9
-
-NO_COLOR=1 npx @tailwindcss/cli -i main.css -o dist/build.css -m \
-&& NO_COLOR=1 ./deno/bin/deno task build
